7a449dad1f2897e5d401b1b9feed12b47f1bf6ac
dimskiyy <petrovskiydima27@gmail.com>
2023-12-13 19:09:06 +0300

    Fix config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
-function App() {
+const App = (): JSX.Element => {
   const [count, setCount] = useState(0)
 
   return (
@@ -30,6 +30,6 @@ function App() {
       </p>
     </>
   )
-}
+};
 
-export default App
+export default App;
