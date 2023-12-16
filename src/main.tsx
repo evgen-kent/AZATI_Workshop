2cdd83f54e96be26fa2a74617d69286fd1d1826a
Alesia Klemenia <klemeny_ap_20@mf.grsu.by>
2023-12-16 20:22:14 +0300

    Add mui theming

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
