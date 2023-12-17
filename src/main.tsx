99717a80c32516da0f4a5ddd20e39aa00d387498
Andrew Petrushkevich <53120843+Miracle-229@users.noreply.github.com>
2023-12-17 17:49:44 +0300

    Created redux (#40)
    * created redux
    
    * resolved problems

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import App from './App.tsx'
+import './index.css'
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <App />
+    <App />
     </ThemeProvider>
-  </React.StrictMode>
-);
+    </Provider>
+  </React.StrictMode>,
+)
