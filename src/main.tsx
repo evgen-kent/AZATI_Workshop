f59d2e382622fc7df629625d15df85a940dc6043
Dmitry Petrovsky <93205162+dimskiyy@users.noreply.github.com>
2023-12-18 20:34:03 +0300

    Create router (#43)

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './App.tsx'
-import './index.css'
 import { Provider } from 'react-redux';
-import { store } from './store/store.ts';
 import { ThemeProvider } from '@emotion/react';
+
+import { store } from './store/store';
+
 import theme from './theme';
+import App from './App'
+
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
     <ThemeProvider theme={theme}>
-    <App />
+      <App />
     </ThemeProvider>
     </Provider>
   </React.StrictMode>,
