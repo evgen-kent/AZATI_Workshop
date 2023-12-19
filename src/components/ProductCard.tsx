76053a1e40e15940cbaf7d70e927eaad23e2e27b
Miracle-229 <zzzzhd02@gmail.com>
2023-12-19 22:56:53 +0300

    create product-card

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,30 @@
+import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
+import style from '../styles/ProductCard.module.scss';
+
+type productCardProps = {
+  img: string;
+  title: string;
+  price: string;
+};
+
+const ProductCard = ({ img, title, price }: productCardProps): JSX.Element => {
+  return (
+    <Card className={style.card}>
+      <CardActionArea>
+        <CardMedia
+          className={style.img}
+          component="img"
+          image={img}
+          alt="green iguana"
+        />
+        {/* здесь должен быть компонент рейтинга */}
+        <CardContent className={style.card_content}>
+          <h3>{title}</h3>
+          <h4>{price}</h4>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default ProductCard;
