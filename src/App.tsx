fca635a1651897191c6251b9029d7802534d3ea9
Kirill <kirilllisovskyi@gmail.com>
2023-12-19 15:10:38 +0300

    Add log in and sign up

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 
 const App = (): JSX.Element => {
-    return (
-        <>
-        <RouterProvider router={router} />
-        </>
-    );
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/login-wrapper/login-wrapper.tsx
@@ -0,0 +1,27 @@
+import SubmitButton from "../button-auth/button-auth";
+import Input from "../input-auth/input-aurh";
+import "./login-wrapper.css";
+import mailIcon from "../../assets/mui-mail.svg";
+import passIcon from "../../assets/mui-lock.svg";
+import { Checkbox } from "@mui/material";
+
+const Login = () => {
+  return (
+    <div className="login-wrapper">
+      <div className="label">LOG IN</div>
+      <Input content="Email" imgSrc1={mailIcon} typee="text"></Input>
+      <Input content="Password" imgSrc1={passIcon} typee="password"></Input>
+      <div className="remember">
+        <Checkbox sx={{ padding: 0 }}></Checkbox>
+        <div className="text-remember">Remember me</div>
+      </div>
+      <SubmitButton content="Log in"></SubmitButton>
+      <p className="footer">
+        <span style={{ color: "#000000" }}>Don’t have an account? </span>
+        <span style={{ color: "#00000099" }}>Sign up</span>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
