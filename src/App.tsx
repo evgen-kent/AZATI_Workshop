3c34a6d4be36c8ce65d4008d6abc6806340d5fe9
Miracle-229 <zzzzhd02@gmail.com>
2023-12-19 15:34:17 +0300

    create-layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 import { RootState } from './store/store';
 import { decrement, increment } from './store/counterSlice';
+import style from './styles/App.module.scss';
+import Layout from './layouts/Layout';
 
 function App() {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
 
-
   return (
-    <div>
-      <div>
+    <Layout title="SHOP.CO">
+      <div className={style.main}>
         <button
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
@@ -25,9 +26,8 @@ function App() {
           Decrement
         </button>
       </div>
-    </div>
+    </Layout>
   );
 }
-
 
 export default App;
