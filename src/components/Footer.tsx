cf7f40c8d6be9da638efdb15122a54fd9f1e453a
Miracle-229 <zzzzhd02@gmail.com>
2023-12-20 11:18:18 +0300

    fix position form in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ import { Button } from '@mui/material';
 const Footer = (): JSX.Element => {
   const id = useId();
   return (
-    <div className={style.footer}>
+    <div className={style.wrapper}>
       <div className={style.form}>
         <p>STAY UPTO DATE ABOUT OUR LATEST OFFERS</p>
         <div
@@ -47,46 +47,48 @@ const Footer = (): JSX.Element => {
           </Button>
         </div>
       </div>
-      <div className={style.list_row}>
-        <div className={style.list_block}>
-          <h3>shop.co</h3>
-          <p>
-            We have clothes that suits your style and which you’re proud to
-            wear. From women to men.
-          </p>
-          <ul className={style.socials_icons}>
-            {footerSocials.map((social) => {
-              const Icon = social.icon;
+      <div className={style.footer}>
+        <div className={style.list_row}>
+          <div className={style.list_block}>
+            <h3>shop.co</h3>
+            <p>
+              We have clothes that suits your style and which you’re proud to
+              wear. From women to men.
+            </p>
+            <ul className={style.socials_icons}>
+              {footerSocials.map((social) => {
+                const Icon = social.icon;
+                return (
+                  <li key={id}>
+                    <Icon />
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          {footerList.map((item) => (
+            <div key={id} className={style.list_block}>
+              <h3 className={style.list_h3}>{item.title}</h3>
+              <ul className={style.list_links}>
+                {item.list.map((listItem, listIndex) => (
+                  <li key={listIndex}>{listItem}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className={style.copy}>
+          <p>© 2000-2021, All rights reserved</p>
+          <ul>
+            {footerCredits.map((social) => {
               return (
                 <li key={id}>
-                  <Icon />
+                  <img src={social.icon} alt="" />
                 </li>
               );
             })}
           </ul>
         </div>
-        {footerList.map((item) => (
-          <div key={id} className={style.list_block}>
-            <h3 className={style.list_h3}>{item.title}</h3>
-            <ul className={style.list_links}>
-              {item.list.map((listItem, listIndex) => (
-                <li key={listIndex}>{listItem}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-      <div className={style.copy}>
-        <p>© 2000-2021, All rights reserved</p>
-        <ul>
-          {footerCredits.map((social) => {
-            return (
-              <li key={id}>
-                <img src={social.icon} alt="" />
-              </li>
-            );
-          })}
-        </ul>
       </div>
     </div>
   );
