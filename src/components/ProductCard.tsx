fdc03c51db23a5d600b4441a6f28578966105cfa
Miracle-229 <zzzzhd02@gmail.com>
2023-12-25 13:04:10 +0300

    connect rating bar to product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,30 +1,32 @@
-import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
-import style from '../styles/ProductCard.module.scss';
+import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material'
+import style from '../styles/ProductCard.module.scss'
+import RatingBar from './UI/RatingBar'
 
 type productCardProps = {
-  img: string;
-  title: string;
-  price: string;
-};
+	img: string
+	title: string
+	price: string
+}
 
 const ProductCard = ({ img, title, price }: productCardProps): JSX.Element => {
-  return (
-    <Card className={style.card}>
-      <CardActionArea>
-        <CardMedia
-          className={style.img}
-          component="img"
-          image={img}
-          alt="green iguana"
-        />
-        {/* здесь должен быть компонент рейтинга */}
-        <CardContent className={style.card_content}>
-          <h3>{title}</h3>
-          <h4>{price}</h4>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
-};
+	return (
+		<Card className={style.card}>
+			<CardActionArea>
+				<CardMedia
+					className={style.img}
+					component='img'
+					image={img}
+					alt='green iguana'
+				/>
+				{/* здесь должен быть компонент рейтинга */}
+				<CardContent className={style.card_content}>
+					<h3>{title}</h3>
+					<RatingBar name='rating' />
+					<h4>{price}</h4>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	)
+}
 
-export default ProductCard;
+export default ProductCard
