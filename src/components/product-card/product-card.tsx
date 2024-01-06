52d557bfba3a56d049484634d03ee216b9de9236
Miracle-229 <zzzzhd02@gmail.com>
2024-01-06 19:46:19 +0300

    resolved problems

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -1,27 +1,28 @@
-import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material'
-import style from './product-card.module.scss'
-import RatingBar from '../UI/rating-bar'
+import {
+	Card,
+	CardActionArea,
+	CardContent,
+	CardMedia,
+	Typography
+} from '@mui/material'
+import style from '../styles/ProductCard.module.scss'
+import { IProduct } from '../../types/productCardType'
 
-type productCardProps = {
-	img: string
-	title: string
-	price: string
-}
-
-const ProductCard = ({ img, title, price }: productCardProps): JSX.Element => {
+const ProductCard = ({ image, name }: IProduct): JSX.Element => {
 	return (
 		<Card className={style.card}>
 			<CardActionArea>
 				<CardMedia
 					className={style.img}
 					component='img'
-					image={img}
+					image={image}
 					alt='green iguana'
 				/>
+				{/* здесь должен быть компонент рейтинга */}
 				<CardContent className={style.card_content}>
-					<h3>{title}</h3>
-					<RatingBar name='read' />
-					<h4>{price}</h4>
+					<Typography variant='subtitle1'>{name}</Typography>
+					{/* в локальную бд не смог добавить поле цены , т.к похоже она не прописана */}
+					<Typography component='span'>120$</Typography>
 				</CardContent>
 			</CardActionArea>
 		</Card>
