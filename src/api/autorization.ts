5573759ebaae22170da884894940fff976ea2006
Miracle-229 <zzzzhd02@gmail.com>
2024-01-06 19:16:35 +0300

    change dir for axios files

--- src/api/autorization.ts
+++ src/api/autorization.ts
@@ -4,19 +4,7 @@ export const api = axios.create({
 	baseURL: 'http://localhost:5000'
 })
 
-axios.interceptors.request.use(
-	async config => {
-		const token = localStorage.getItem('accessToken')
-		if (token) {
-			config.headers['Authorization'] = 'Bearer ' + token
-		}
-		return config
-	},
-	error => {
-		Promise.reject(error)
-	}
-)
-axios.interceptors.request.use(
+api.interceptors.request.use(
 	async config => {
 		const token = localStorage.getItem('accessToken')
 		if (token) {
