50420da196cdf019f62ccbe579f7922196d9e556
dimskiyy <petrovskiydima27@gmail.com>
2024-01-10 22:27:01 +0300

    Create ProductsRouter

---
+++ src/pages/Products/index.tsx
@@ -0,0 +1,29 @@
+import Layout from './../../layouts/Layout'
+import ProductCard from '../../components/product-card/product-card'
+import { IProduct } from '../../types/productCardType'
+import { productsData } from '../../store/product/productSelector'
+import { useSelector } from 'react-redux'
+import style from './Products.module.scss'
+
+
+const Products = (): JSX.Element => {
+    const products = useSelector(productsData)
+
+    return (
+        <div>
+            <Layout title='Products'>
+                <div className={style.products}>
+                        {products.map((product: IProduct) => (
+                            <ProductCard
+                                key={product.id}
+                                name={product.name}
+                                image={product.image}
+                            />
+                        ))}
+                </div>
+            </Layout>   
+        </div>
+    )    
+}
+
+export default Products;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
+import Products from './pages/Products'
 
 const router = createBrowserRouter([
 	{
 		path: '/AZATI_Workshop',
 		element: <Home />
+	},
+	{
+		path: '/AZATI_Workshop/Products',
+		element: <Products />
 	}
 ])
 
