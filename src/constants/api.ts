400c6b5ab146f32fa156072b7a0fb4d58db00885
Andrew Petrushkevich <53120843+Miracle-229@users.noreply.github.com>
2024-01-10 18:17:33 +0300

    Create axios (#85)
    * create axios file
    
    * create interceptors
    
    * change dir for axios files
    
    * create actions for login and registration
    
    * resolved path for redux

--- src/constants/api.ts
+++
@@ -1,3 +0,0 @@
-export const api = 'http://localhost:3000/'
-export const token =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNjcmlwdDQiLCJzdWIiOiI2NThjMzJlZDE4MjZlNDZlOTg2NDdiZTEiLCJpYXQiOjE3MDM2ODY5Nzl9.59M8Pfy3lZTE3ZAoVS1l7WkOt8DupqJEmq9HrDm7rjU'
