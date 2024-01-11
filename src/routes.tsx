5998c21e1faedea5197ecd42bb3a48434ae69e4f
Alesia Klemenia <klemeny_ap_20@mf.grsu.by>
2024-01-11 22:17:25 +0300

    Fix routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,7 +8,7 @@ const router = createBrowserRouter([
 		element: <Home />
 	},
 	{
-		path: `/AZATI_Workshop/product/:productId`,
+		path: `/AZATI_Workshop/products/:productId`,
 		element: <Product />
 	}
 ])
