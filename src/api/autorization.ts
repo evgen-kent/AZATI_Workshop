d435ab236ee4b807d13debe07019ba292a49ce0f
Miracle-229 <zzzzhd02@gmail.com>
2024-01-11 15:12:00 +0300

    add mock back

--- src/api/autorization.ts
+++ src/api/autorization.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-	baseURL: `${import.meta.env.VITE_SERVICE_URL}`
+	baseURL: `https://azati-workshop-be.fly.dev/`
 })
 
 api.interceptors.request.use(
