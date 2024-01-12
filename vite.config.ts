28e6ac4f8976aaa988883baf05f817ee2dfc2a03
Miracle-229 <zzzzhd02@gmail.com>
2024-01-12 13:38:43 +0300

    second change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-	plugins: [
-		react(),
-		svgr({
-			include: '**/*.svg?react'
-		})
-	],
-	server: {
-		port: 5173,
-		host: '127.0.0.1'
-	},
-	base: '/AZATI_Workshop/'
-})
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+	return defineConfig({
+		plugins: [
+			react(),
+			svgr({
+				include: '**/*.svg?react'
+			})
+		],
+		server: {
+			port: 5173,
+			host: '127.0.0.1'
+		},
+		base: '/AZATI_Workshop/'
+	})
+}
+// https://vitejs.dev/config/
 
