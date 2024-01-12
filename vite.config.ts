43f3167769061d262ad2b29914eff0bf7f5f2401
Andrew Petrushkevich <53120843+Miracle-229@users.noreply.github.com>
2024-01-12 13:34:08 +0300

    Add mock back (#103)
    * add mock-back
    
    * fix problems
    
    * delete non-authorization and rename & fix authorization
    
    * change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
-	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-	return defineConfig({
-		plugins: [
-			react(),
-			svgr({
-				include: '**/*.svg?react'
-			})
-		],
-		server: {
-			port: 5173,
-			host: '127.0.0.1'
-		},
-		base: '/AZATI_Workshop/'
-	})
-}
-// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		react(),
+		svgr({
+			include: '**/*.svg?react'
+		})
+	],
+	server: {
+		port: 5173,
+		host: '127.0.0.1'
+	},
+	base: '/AZATI_Workshop/'
+})
+
